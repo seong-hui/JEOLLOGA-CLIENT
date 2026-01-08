b5cfd2a563d2cf3c8ca4cb7d4e2b953c8dbc7081
gahyeon <gus_05@naver.com>
2026-01-08 15:54:55 +0900

    feat: png 이미지 타입 선언 추가

--- src/img.d.ts
+++ src/img.d.ts
@@ -2,3 +2,10 @@ declare module '*.svg' {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+
+declare module '*.png' {
+  const value: {
+    src: string;
+  };
+  export default value;
+}
