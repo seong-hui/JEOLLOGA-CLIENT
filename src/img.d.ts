e11d895c38acf8a849fe1d890421fccab41f0b3f
gahyeon <gus_05@naver.com>
2026-01-10 17:57:47 +0900

    fix: img.d.ts 내 png 모듈 타입 선언 추가

--- src/img.d.ts
+++ src/img.d.ts
@@ -2,3 +2,9 @@ declare module '*.svg' {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+
+declare module '*.png' {
+  import { StaticImageData } from 'next/image';
+  const content: StaticImageData;
+  export default content;
+}
