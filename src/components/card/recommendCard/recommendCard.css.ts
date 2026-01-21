1d8bd8e7c990106c3abcb378d5f9eab3529e613a
moonseonghui <msh9980@naver.com>
2026-01-21 16:16:42 +0900

    feat: RecommendCard 컴포넌트 구현

---
+++ src/components/card/recommendCard/recommendCard.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+const container = style({
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '7px',
+  width: '100%',
+  marginTop: '1.2rem',
+  marginBottom: '5.4rem',
+});
+
+export default { container };
